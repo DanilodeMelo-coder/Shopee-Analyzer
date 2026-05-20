9a3a8a8d258f58001ee4743b1e90f4b7fdc6aaf6
unknown <danilo.demelo.0207@gmail.com@gmail.com>
2026-05-20 15:44:31 -0300

    Fix: bug do desaparecimento do service_upload resolvido

--- app/services/service_upload.py
+++ app/services/service_upload.py
@@ -1,21 +1,25 @@
 import pandas as pd
-import io #tratar sem precisar salvar na memoria
+import io
 import math
 
-#verificar se valor é nan ou float e troca para str
 def limpar_valor(v):
     if isinstance(v, float) and math.isnan(v):
         return ""
     return v
 
+def encontrar_inicio(conteudo: bytes) -> int:
+    linhas = conteudo.decode("utf-8-sig").splitlines()
+    for i, linha in enumerate(linhas):
+        if linha.startswith("#,") or linha.startswith("#\t"):
+            return i
+    return 7
 
 def ler_planilha(conteudo: bytes, filename: str) -> pd.DataFrame:
     if filename.endswith(".csv"):
-        df = pd.read_csv(io.BytesIO(conteudo),  skiprows=7, encoding="utf-8-sig")
+        df = pd.read_csv(io.BytesIO(conteudo), skiprows=encontrar_inicio(conteudo), encoding="utf-8-sig")
     else:
-        df = pd.read_excel(io.BytesIO(conteudo),  skiprows=7)
+        df = pd.read_excel(io.BytesIO(conteudo), skiprows=7)
 
     df = df.apply(lambda col: col.map(limpar_valor))
 
     return df
-
