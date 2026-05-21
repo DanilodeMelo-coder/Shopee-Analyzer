5b870f1c76839170d06bd310f64fa6daeea8659d
unknown <danilo.demelo.0207@gmail.com>
2026-05-21 10:48:01 -0300

    Feat: Criação do service de taxa de converção

--- app/services/service_analyzer.py
+++ app/services/service_analyzer.py
@@ -6,3 +6,28 @@ def analisar_ctr(df: pd.dataFrame) -> list:
     ruins = df[ctr > 2.0]
 
     return ruins[["Nome do Anúncio", "CTR"]].to_dict(orient="records")
+
+
+def analisar_taxaconversao(df: pd.dataFrame) -> list:
+    tx_conversao = df["Taxa de conversão"].str.replace("%", "").astype(float)
+
+    df = df.copy()
+    df["classificacao"] = tx_conversao.apply(classificar_taxa)
+
+    ruins = df[df["classificacao"] == "Ruim"]
+
+    return ruins[["Nome do Anúncio", "Taxa de conversão", "classificacao"]].to_dict(orient="records")
+
+
+    
+
+
+def classificar_taxa(tx: float) -> str:
+    if tx < 1:
+        return "Ruin"
+    elif tx > 1 and tx < 2:
+        return "Aceitavel"
+    elif tx > 2 and tx < 4:
+        return "Bom"
+    else:
+        return "Excelente"
